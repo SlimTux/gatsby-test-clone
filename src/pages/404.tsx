0ded3a32593f12be6ee227bbbd8eb44b8c7a43af
SlimTux <117015097+SlimTux@users.noreply.github.com>
2023-01-22 02:17:34 +0000

    first modify

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 
 const NotFound = (_props: PageProps) => (
   <Layout>
-    <h1>404 - Page Not Found</h1>
+    <h1>404 - Where is the page?°.°</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
   </Layout>
 )
